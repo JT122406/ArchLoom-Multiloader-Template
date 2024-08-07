982e1c2a403b0573afc6e0754d130afc1aafdc72
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2024-08-07 18:06:41 -0400

    Update to new model, add neoforge.mods.toml
    Signed-off-by: Joseph T. McQuigg <J.T.McQuigg12@gmail.com>

--- neoforge/build.gradle.kts
+++ neoforge/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("com.github.johnrengelman.shadow")
 }
 
 architectury {
@@ -11,17 +11,32 @@ val minecraftVersion = project.properties["minecraft_version"] as String
 
 configurations {
     create("common")
-    create("shadowCommon")
+    "common" {
+        isCanBeResolved = true
+        isCanBeConsumed = false
+    }
+    create("shadowBundle")
     compileClasspath.get().extendsFrom(configurations["common"])
     runtimeClasspath.get().extendsFrom(configurations["common"])
     getByName("developmentNeoForge").extendsFrom(configurations["common"])
+    "shadowBundle" {
+        isCanBeResolved = true
+        isCanBeConsumed = false
+    }
 }
 
 loom {
     accessWidenerPath.set(project(":common").loom.accessWidenerPath)
 
+    forge {
+        convertAccessWideners.set(true)
+        extraAccessWideners.add(loom.accessWidenerPath.get().asFile.name)
 
-    // NroForge Datagen Gradle config.  Remove if not using NeoForge datagen
+        mixinConfig("examplemod-common.mixins.json")
+        mixinConfig("examplemod.mixins.json")
+    }
+
+    // Forge Datagen Gradle config.  Remove if not using Forge datagen
     runs.create("datagen") {
         data()
         programArgs("--all", "--mod", "examplemod")
@@ -34,63 +49,26 @@ dependencies {
     neoForge("net.neoforged:neoforge:${project.properties["neoforge_version"]}")
 
     "common"(project(":common", "namedElements")) { isTransitive = false }
-    "shadowCommon"(project(":common", "transformProductionNeoForge")) { isTransitive = false }
+    "shadowBundle"(project(":common", "transformProductionNeoForge"))
 }
 
 tasks {
-    base.archivesName.set(base.archivesName.get() + "-NeoForge")
     processResources {
         inputs.property("version", project.version)
 
-        filesMatching("META-INF/mods.toml") {
+        filesMatching("META-INF/neoforge.mods.toml") {
             expand(mapOf("version" to project.version))
         }
     }
 
     shadowJar {
-        exclude("fabric.mod.json")
-        configurations = listOf(project.configurations.getByName("shadowCommon"))
+        exclude("architectury.common.json")
+        configurations = listOf(project.configurations.getByName("shadowBundle"))
         archiveClassifier.set("dev-shadow")
     }
 
     remapJar {
         inputFile.set(shadowJar.get().archiveFile)
         dependsOn(shadowJar)
-    }
-
-    jar.get().archiveClassifier.set("dev")
-
-    sourcesJar {
-        val commonSources = project(":common").tasks.sourcesJar
-        dependsOn(commonSources)
-        from(commonSources.get().archiveFile.map { zipTree(it) })
-    }
-}
-
-components {
-    java.run {
-        if (this is AdhocComponentWithVariants)
-            withVariantsFromConfiguration(project.configurations.shadowRuntimeElements.get()) { skip() }
-    }
-}
-
-publishing {
-    publications.create<MavenPublication>("mavenNeoForge") {
-        artifactId = "${project.properties["archives_base_name"]}" + "-NeoForge"
-        from(components["java"])
-    }
-
-    repositories {
-        mavenLocal()
-        maven {
-            val releasesRepoUrl = "https://example.com/releases"
-            val snapshotsRepoUrl = "https://example.com/snapshots"
-            url = uri(if (project.version.toString().endsWith("SNAPSHOT") || project.version.toString().startsWith("0")) snapshotsRepoUrl else releasesRepoUrl)
-            name = "ExampleRepo"
-            credentials {
-                username = project.properties["repoLogin"]?.toString()
-                password = project.properties["repoPassword"]?.toString()
-            }
-        }
     }
 }
