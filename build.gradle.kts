e6bcf77f2f16b5a193bf2b499bd593e037719a1f
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2024-08-08 09:19:04 -0400

    Java 21

--- build.gradle.kts
+++ build.gradle.kts
@@ -50,12 +50,12 @@ subprojects {
     java {
         withSourcesJar()
 
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
 
     tasks.withType<JavaCompile>().configureEach {
-        options.release.set(17)
+        options.release.set(21)
     }
 
     publishing {
