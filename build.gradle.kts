c0673630bab508bae9f21277334ff72bef9711ec
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2024-11-20 16:17:55 -0500

    Move Parchment to 1.21.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import net.fabricmc.loom.api.LoomGradleExtensionAPI
 plugins {
     id("architectury-plugin") version "3.4-SNAPSHOT"
     id("dev.architectury.loom") version "1.7-SNAPSHOT" apply false
-    id("com.gradleup.shadow") version "8.3.3" apply false
+    id("com.gradleup.shadow") version "8.3.5" apply false
     java
     idea
     `maven-publish`
@@ -41,10 +41,10 @@ subprojects {
         "minecraft"("com.mojang:minecraft:$minecraftVersion")
         "mappings"(loom.layered{
             officialMojangMappings()
-            parchment("org.parchmentmc.data:parchment-1.21:${project.properties["parchment"]}@zip")
+            parchment("org.parchmentmc.data:parchment-$minecraftVersion:${project.properties["parchment"]}@zip")
         })
 
-        compileOnly("org.jetbrains:annotations:26.0.0")
+        compileOnly("org.jetbrains:annotations:26.0.1")
     }
 
     java {
