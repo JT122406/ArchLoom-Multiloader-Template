04bc240d71a1e9017102c7420a4ed40f86bcb52d
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2025-04-09 17:15:03 -0400

    Loom 1.10, Gradle 8.13, update loaders to latest

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import net.fabricmc.loom.api.LoomGradleExtensionAPI
 
 plugins {
     id("architectury-plugin") version "3.4-SNAPSHOT"
-    id("dev.architectury.loom") version "1.9-SNAPSHOT" apply false
+    id("dev.architectury.loom") version "1.10-SNAPSHOT" apply false
     id("com.gradleup.shadow") version "8.3.6" apply false
     java
     idea
