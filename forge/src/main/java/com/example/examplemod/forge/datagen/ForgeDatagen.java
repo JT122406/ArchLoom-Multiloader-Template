76296277ad060bf0e72c13f6f12e17c3a62e3347
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2025-09-12 23:51:40 -0400

    1.21.8

--- forge/src/main/java/com/example/examplemod/forge/datagen/ForgeDatagen.java
+++ forge/src/main/java/com/example/examplemod/forge/datagen/ForgeDatagen.java
@@ -2,7 +2,7 @@ package com.example.examplemod.forge.datagen;
 
 import com.example.examplemod.ExampleMod;
 import net.minecraftforge.data.event.GatherDataEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.eventbus.api.listener.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, modid = ExampleMod.MOD_ID)
