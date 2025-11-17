e3b71a15219c70f94ada4bf9648e6765e93dc22a
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2025-11-16 21:04:34 -0500

    Gradle 9.2.0, Loom 1.13, Fix NeoForge Datagen Deprecation

--- neoforge/src/main/java/com/example/examplemod/neoforge/datagen/NeoForgeDatagen.java
+++ neoforge/src/main/java/com/example/examplemod/neoforge/datagen/NeoForgeDatagen.java
@@ -5,7 +5,7 @@ import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.data.event.GatherDataEvent;
 
-@EventBusSubscriber(bus = EventBusSubscriber.Bus.MOD, modid = ExampleMod.MOD_ID)
+@EventBusSubscriber(modid = ExampleMod.MOD_ID)
 class NeoForgeDatagen {
 
     @SubscribeEvent
